bc02185e5ffd39496274fec194adfa8160a924e0
Maya73939133 <158637741+Maya73939133@users.noreply.github.com>
2024-02-10 11:31:10 -0500

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "bMI6MYdTV5XpIhjNQY6P49lCPZQFdTK4oq2kcFjioXCuXZj5i3rUCxzMMEDUcS6j";
 /**
  * Get list of teams in event
  *
